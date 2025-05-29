2a3818966e053304d2c72e19a2093e6d15de01cb
mantakanusaiba <123790428+mantakanusaiba@users.noreply.github.com>
2025-05-29 04:55:47 +0000

    Add related products

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -91,6 +91,21 @@ const ProductDetails = () => {
                     </div>
                 </div>
             </div>
+            <div className="flex flex-col items-center mt-20">
+            <div className="flex flex-col items-center w-max">
+                <p className="text-3xl font-medium">Related Products</p>
+                <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4
+            gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
+                {relatedProducts.filter((product)=>product.inStock).map
+                ((product,index)=>(
+                   <ProductCard key={index} product={product}/>
+                ))}
+            </div>
+            <button onClick={()=>{navigate('/product'); scrollTo(0,0)}} className="mx-auto cursor-pointer px-12 my-16 py-2.5 border
+            rounded text-primary hover:bg-primary/10 transition">See More</button>
+        </div>
      </div>
     );
 };
